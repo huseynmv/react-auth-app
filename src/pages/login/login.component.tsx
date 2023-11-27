4b516e5969ab9aedf9298abbdf6ce23870bd9197
huseynmv <huseynmv@yahoo.com>
2023-11-27 13:20:03 +0400

    login form

--- src/pages/login/login.component.tsx
+++ src/pages/login/login.component.tsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Button, Card, Form, Input } from "antd";
+import { useLoginStyles } from "./login.style";
+import bgLeft from "../../assets/images/statics/background-left.png";
+import bgRight from "../../assets/images/statics/background-right.png";
 
 const LoginComponent = () => {
-  return <div>LoginComponent</div>;
+  const styles = useLoginStyles();
+
+  return (
+    <div className={styles.page}>
+      <div className={styles.wrapper}>
+        <div className={styles.left}>
+          <img src={bgLeft} alt="" />
+        </div>
+        <div className={styles.right}>
+          <img src={bgRight} alt="" />
+        </div>
+      </div>
+      <div className={styles.form}>
+        <div className={styles.title}>
+          <p>Welcome to the site</p>
+        </div>
+        <p className={styles.subTitle}>Sign In</p>
+        <Form name="login" layout="vertical">
+          <Form.Item
+            style={{ marginTop: 38 }}
+            name="Email"
+            label="Enter your username or email address"
+          >
+            <Input placeholder="Username or email" type="email" />
+          </Form.Item>
+          <Form.Item
+            style={{ marginTop: 38 }}
+            name="Password"
+            label="Enter your username or email address"
+          >
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+          <div>
+            <Button className="w-100" type="primary" htmlType="submit">
+              Sign In
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginComponent;
---
+++ src/pages/login/login.style.ts
@@ -0,0 +1,52 @@
+import { createUseStyles } from "react-jss";
+import colors from "../../assets/styles/abstracts/color";
+import { rem } from "../../assets/styles/abstracts/functions";
+
+const styles = {
+  page: {},
+  left: {
+    backgroundColor: "#ECBC76",
+    minHeight: "100vh",
+    width: "50%",
+    justifyContent: "center",
+  },
+  right: {
+    backgroundColor: "#FFFFFF",
+    minHeight: "100vh",
+    width: "40%",
+  },
+  wrapper: { display: "flex" },
+  form: {
+    position: "absolute",
+    transform: "translate(-50%, -50%)",
+    top: "50%",
+    left: "50%",
+    zIndex: 999,
+    boxShadow: "0px 4px 35px rgba(0, 0, 0, 0.88 )",
+    borderRadius: "40px",
+    padding: "50px 40px",
+    backgroundColor: "white",
+  },
+  title: {
+    color: "black",
+    fontSize: 20,
+    fontWeight: "400",
+    // alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "0px 188px",
+    // word-wrap: break-word
+  },
+  subTitle: {
+    color: "black",
+    fontSize: 55,
+    fontWeight: "500",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "0px 177px",
+  },
+};
+
+export const useLoginStyles = createUseStyles(styles);
